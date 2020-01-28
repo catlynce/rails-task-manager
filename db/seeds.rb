cb7782c869424ab8c66e3344f01389acf7b639de
Nuno Martins <nuno@alvesmartins.eu>
2020-01-28 21:11:50 +0000

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create title: 'Buy eggs', details: 'Get fresh ones'
+Task.create title: 'Study', details: 'Do a bunch of flashcards!', completed: true
+p "Success"

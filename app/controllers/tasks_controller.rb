a88079d8d29c85017ac75480c788e969e032deab
Nuno Martins <nuno@alvesmartins.eu>
2020-01-28 20:57:10 +0000

    Add show, new, create and edit routes, views and actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,27 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create task_params
+    redirect_to action: "index"
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
